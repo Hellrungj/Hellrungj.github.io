34628b25eee3f7b44c1e26937e362f7d37951f06
Hellrungj <johnjameshellrung@gmail.com>
2018-10-27 02:29:07 -0500

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Hellrungj.github.io
-Personal Resume Website
-
-Includes an index.html page along with a CSS, PDF and image files.
-
-## Contact Info:
-Email: [johnjameshelrung@gmail.com](johnjameshellrung@gmail.com)
---
+++ precache-manifest.03124fbee7801eafc81b04793a286b9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1af115fe0a474b75545aeb6968b2f8a0",
+    "url": "/static/media/profile.1af115fe.png"
+  },
+  {
+    "revision": "e0468d6857120d7b4fa0516d92bf4196",
+    "url": "/static/media/Resume-John-Hellrung.e0468d68.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40526706da8aded7fa09",
+    "url": "/static/js/main.40526706.chunk.js"
+  },
+  {
+    "revision": "88c3a369afb9c41f48b7",
+    "url": "/static/js/1.88c3a369.chunk.js"
+  },
+  {
+    "revision": "40526706da8aded7fa09",
+    "url": "/static/css/main.770d5100.chunk.css"
+  },
+  {
+    "revision": "88c3a369afb9c41f48b7",
+    "url": "/static/css/1.15326e7d.chunk.css"
+  },
+  {
+    "revision": "4562078d2e6d0514d0a853102ee69716",
+    "url": "/index.html"
+  }
+];
